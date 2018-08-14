ca735f73da760cdd6eee7f513806f39453a75ec3
simba999 <oliverking8985@yahoo.com>
2018-08-14 14:22:33 -0400

    add codes for converting canvas to image

--- src/test/testUI.spec.js
+++ src/test/testUI.spec.js
@@ -8,28 +8,28 @@ const until = webdriver.until;
 require("geckodriver");
 
 // Application Server
-const serverUri = "http://localhost:3000/";
-const appTitle = "Custom Star Map Editor";
+const serverUri = 'http://localhost:3000/';
+const appTitle = 'Custom Star Map Editor';
 
 /**
  * Config for Chrome browser
  * @type {webdriver}
  */
-let browser = new webdriver.Builder()
- .usingServer()
- .withCapabilities({ browserName: "chrome" })
- .build();
+const browser = new webdriver.Builder()
+  .usingServer()
+  .withCapabilities({ browserName: 'chrome' })
+  .build();
 
 
- // /**
- //  * Config for Firefox browser (Comment Chrome config when you intent to test in Firefox)
- //  * @type {webdriver}
- //  */
- 
- // const browser = new webdriver.Builder()
- //  .usingServer()
- //  .withCapabilities({ browserName: "firefox" })
- //  .build();
+// /**
+//  * Config for Firefox browser (Comment Chrome config when you intent to test in Firefox)
+//  * @type {webdriver}
+//  */
+
+// const browser = new webdriver.Builder()
+//  .usingServer()
+//  .withCapabilities({ browserName: "firefox" })
+//  .build();
   
 
 /**
@@ -37,54 +37,59 @@ let browser = new webdriver.Builder()
 * @return {[type]} [description]
 */
 function logTitle() {
-	return new Promise((resolve, reject) => {
-		browser.getTitle().then((title) => {
-			resolve(title);
-		});
-	});
+  return new Promise((resolve) => {
+    browser.getTitle().then((title) => {
+      resolve(title);
+    });
+  });
 }
 
-describe("Home Page", () => {
-	/* Configuration before launching a case */
-	beforeEach(function() {
-    window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 90000;
+describe('Home Page', () => {
+  /* Configuration before launching a case */
+  beforeEach(() => {
+    window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
   });
 
-	/**
-	  * Test case to load our application and check the title.
-	  */
-	it("Should load the home page", async function() {
-		// return new Promise((resolve, reject) => {
-		await browser
-				.get(serverUri)
-				.then(logTitle)
-				.then(async (title) => {
-					console.log(title)
-					assert.strictEqual(title, appTitle, "App is successfully loaded!!!");
+  /**
+  * Test case to load our application and check the title.
+  */
+  it('Should load the home page', async () => {
+    // return new Promise((resolve, reject) => {
+    await browser
+      .get(serverUri)
+      .then(logTitle)
+      .then(async (title) => {
+        assert.strictEqual(title, appTitle, 'App Loading Failed!!!');
 
-					// await browser.wait(until.titleIs(appTitle), 10000);
+        // await browser.wait(until.titleIs(appTitle), 10000);
+        await browser.wait(until.elementLocated(By.xpath("//canvas[@id='canvas']")));
 
-					// const canvas = browser.findElement(By.xpath("//canvas[@id='canvas']"))
-					// 	.then((obj) => {
-					// 		console.log(obj)
-					// 	});
+        // get theme image buttons
+        const themeBtns = await browser.findElements(By.xpath("//li[contains(@class, 'theme')]"));
 
+        assert.strictEqual(
+          themeBtns.length,
+          4,
+          'There should be 4 theme buttons on page',
+        );
 
-					// await browser.findElement(By.xpath("//li[contains(@class, 'theme')]"))
-					// 	.then(async (obj) => {
-					// 		const themeColor = await obj.getText().then((text) => { 
-					// 			return text 
-					// 		});
-					// 		console.log('color: ', themeColor);
+        // iteratate and click theme image buttons and check canvas change
+        await browser.findElements(By.xpath("//li[contains(@class, 'theme')]"))
+          .then(async (objs) => {
+            await objs.forEach(async (element) => {
+              await element.click();
+              await setTimeout(() => {}, 3000);
 
-					// 		// await obj.click()
-					// 		// await setTimeout(()=> {}, 3000);
-					// 	})
-
-					// browser.quit();
-					resolve();
-				})
-				.catch(err => console.log(err));
-		})
-	// });
+              await browser.findElement(By.xpath("//canvas[@id='canvas']"))
+                .then((canvasObject) => {
+                  const base = browser.executeScript('return arguments[0].toDataURL();', canvasObject);
+                  base.then((result) => {
+                    console.log(result);
+                  })
+                });
+            });
+          });
+      })
+      .catch(err => console.log(err));
+  });
 });
